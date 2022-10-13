8326379eab8a9e0a8ebc6972c03185d53ba04def
lidrail35 <lidrail35@mail.ru>
2022-10-13 21:17:55 +0300

    feat: add solve what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  //console.log('+', date, typeof date, date instanceof Date);
+  //console.log('++', date, typeof date.getUTCMonth, date.hasOwnProperty('getUTCMonth'));
+  if (typeof date === 'undefined') {return 'Unable to determine the time of year!'}
+  if (!(date instanceof Date)) {throw new Error('Invalid date!');}
+  if (date.hasOwnProperty('toString')) {throw new Error('Invalid date!');}
+  //Getownpropertynames?  GetOwnPropertySymbols
+  //if (Object.getOwnPropertyNames(date)) {throw new Error('Invalid date!');}
+  //if ( !(date.hasOwnProperty('getUTCMonth')) ) {throw new Error('Invalid date!');}
+//  if (!(date instanceof Date)) {return('Invalid date!');}  
+
+  let month = date.getMonth();
+  let result = (month >=2 && month <= 4) ? 'spring' :
+               (month >=5 && month <= 7) ? 'summer' :
+               (month >=8 && month <= 10) ? 'autumn' : 'winter';
+// remove line with error and write your code here
+return result;
 }
 
 module.exports = {
